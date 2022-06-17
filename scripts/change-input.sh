2f4722eaa2bb99056bda0c5e7f0672658705a31e
Paulo Ditarso <paulo.maciel@ifpb.edu.br>
2022-06-16 22:00:46 -0300

    POSITRON: organizing scratch folder
    (cherry picked from commit 2906a44c95f10762dffacfc87f2ecea553d8eed1)

---
+++ scripts/change-input.sh
@@ -0,0 +1,23 @@
+# HOW TO USE THIS SCRIPT:
+# FROM THE MAIN FOLDER OF POSITRON, CALL FOR IT IN THE SCRIPTS FOLDER, PASSING THE NUMBER OF NODES AS ARGUMENT, SUCH AS
+#
+# $ bash scripts/change-input.sh 60nodes
+#
+# OR
+#
+# $ bash scripts/change-input.sh 150nodes
+#
+# VALID OPTIONS OF ARGUMENTS: 60nodes, 90nodes, 120nodes, 150nodes, 180nodes
+
+numberofnodes=$1
+
+options='60nodes 90nodes 120nodes 150nodes 180nodes'
+
+if [[ " $options " =~ .*\ $numberofnodes\ .* ]]; then
+    cd scratch/
+    rm input.yaml
+    ln -s ./$numberofnodes/input.yaml input.yaml  
+else
+    echo "invalid option"
+fi
+
